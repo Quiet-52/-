823c8dcf4de0c708130655deada7d667a4554c85
Quiet-52 <81682632+Quiet-52@users.noreply.github.com>
2021-05-26 11:32:29 +0300

    Add files via upload

---
+++ &&&.pas
@@ -0,0 +1,13 @@
+﻿var a: string;
+begin
+writeln('как тебя зовут?');
+readln(a);
+writeln('выдан отделом УФМС?');
+readln();
+writeln('серия паспорта?');
+readln();
+writeln('год рождения?');
+readln();
+writeln('страна проживания');
+readln();
+end.
